5f8553d49cba20b65b60dc30102fae5b76c56bb6
dbehmoaras@gmail.com <dbehmoaras@gmail.com>
2021-05-12 18:22:41 -0400

    successfully pushed the message to the front end using the socket

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const kafka = require('./kafka');
 const kafkaApp = express();
 // const testRouter = express.Router();
 
+// socket.io with Express
 
 const path = require("path");
 
@@ -57,32 +58,52 @@ const io = require('socket.io')(server, {
 });
 
 //connect the consumer to the kafka cluster
-consume((message) => {
-  io.sockets.emit('newMessage', message)
-  // console.log('socket emit message ', message)
-  // let messageValue = JSON.stringify(message.value);
-  // let messageKey = JSON.stringify(message.key);
+
+
+consume(message => {
   let messageValue = message.value.toString('utf-8');
-  let messageKey = message.key.toString('utf-8');
+  console.log('socket emit message ', messageValue)
+  io.on('connection', (socket) => {
+    // let messageValue = message.value;
+
+    socket.emit("newMessage", messageValue)
+      // .then(arg => console.log('***THEN arg:', arg))
+      // .catch((err) => console.log('error in emitter, ' + err))
+  })
+    // .then( io.on('newMessage', console.log('received message:', message)))
+    // .catch(err => console.log('***ERR', err))
+  // console.log('socket emit message ', message)
+  // let messageKey = JSON.stringify(message.key);
+  // let messageKey = message.key.toString('utf-8');
   // console.log('*** messageValue', messageValue);
   // console.log('*** messageKey', messageKey);
-
+  // io.on('newMessage', () => console.log('****Jumping Jehozavat'))
 })
 
-io.on('connection', client => {
-  console.log('server.js: IO Connected');
-  // client.emit('client emitting')
-  //this is where the error is
-  // consumer.on(consumerEvents.REQUEST, function (message) {
-  //   // console.log('*** IS THIS THE INSTRUMENT',message);
-  //   client.emit('event', message.value);
-  //   console.log("MESSAGE VALUE", message);
-  // });
-  client.on('newMessage',(message) => {
-    console.log('*** MESSAGE SENT:', message.value.toString('utf-8'))
-  })
-  client.on('disconnect', () => {
-    console.log('Client disconnected');
-  });
-});
+
+// io.on('connection', (socket) => {
+//   socket.on('newMessage', (arg) => {
+//     console.log('listener argument', arg)
+//   })
+//   console.log('this should appear after socket emit');
+// })
+
+// io.on('connection', (client) => {
+//   console.log('server.js: IO Connected');
+//   // console.log('*** client', client.client)
+//   // client.emit('client emitting')
+//   //this is where the error is
+//   // consumer.on(consumerEvents.REQUEST, function (message) {
+//     //   // console.log('*** IS THIS THE INSTRUMENT',message);
+//     //   client.emit('event', message.value);
+//     //   console.log("MESSAGE VALUE", message);
+//     // });
+
+//   client.on('newMessage', (message) => {
+//     console.log('*** MESSAGE SENT:', message.value.toString('utf-8'))
+//   })
+//   client.on('disconnect', () => {
+//     console.log('Client disconnected');
+//   });
+// });
 
