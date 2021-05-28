c3fec4dc408596fc775da86f42baaeab0041d127
dbehmoaras@gmail.com <dbehmoaras@gmail.com>
2021-05-28 13:11:43 -0400

    fixed all comments with proper @param notation. added useInterval hook to client class

--- srv-lib/AtomicKafkaServer.js
+++ srv-lib/AtomicKafkaServer.js
@@ -1,13 +1,13 @@
 /**
  * Defines the AtomicKafkaServer class, which connects a local or cloud-hosted Apache Kafka cluster
  * to user-defined applications as producers or consumers.
- * @kafkaServer : Node.js server instance that listens on a user-specified port
+ * @param {Node.js Server Instance} kafkaServer:listens on a user-specified port
  */
 
  const { Kafka } = require('kafkajs');
  const Consumer = require('./consumer.js');
  const Producer = require('./producer.js');
- 
+
 
 class AtomicKafkaServer {
 	constructor(kafkaServer){
@@ -20,30 +20,30 @@ class AtomicKafkaServer {
 			}
 		});
 	};
-	
+
 	/**
-	 * Creates a new instance of a Consumer class and stores it in the Consumers object of this class. 
-	 * @groupId : string, user-defined groupId
+	 * Creates a new instance of a Consumer class and stores it in the Consumers object of this class.
+	 * @param {string} groupId: user-defined groupId
 	 */
 	newConsumer(groupId){
 		this.Consumers[groupId] = new Consumer(groupId);
 	};
-	
-	/** 
-	 * Creates a new instance of a Producer class and stores it in the Producers object of this class. 
-	 * @topic : string, user-defined topic that already exists on the cluster
-	 * STRETCH FEATURE: this will also allow the user to create a new topic using an admin broker and the createTopics() method in the KafkaJS library
+
+	/**
+	 * Creates a new instance of a Producer class and stores it in the Producers object of this class.
+	 * @param {string} topic: user-defined topic that already exists on the cluster
+	 * STRETCH FEATURE: this will also allow the user to create a new topic using an admin broker and the createTopics() method in the KafkaJS library. Will require a hashed key to access the admin functions required to add topics.
 	 */
 	newProducer(topic){
 		this.Producers[topic] = new Producer(topic);
 	};
 
 	/**
-	 * socketConsume emits messages that are consumed by the consumer, these messages can be received through sockets listening for the message. 
+	 * socketConsume emits messages that are consumed by the consumer, these messages can be received through sockets listening for the message.
 	 * socketConsume is invoked on the server side to emit the events and send the payload to the client interface
-	 * @groupId : string, the group that identifies which consumer the socket will be emitting data from
-	 * @topic : string, the topic that exists in the cluster and will specify where the consumer will take messages
-	 * @event : string, the event that the socket will use to emit the consumed message
+	 * @param {string} groupId: the group that identifies which consumer the socket will be emitting data from
+	 * @param {string} topic: the topic that exists in the cluster and will specify where the consumer will take messages
+	 * @param {string} event: the event that the socket will use to emit the consumed message
 	 */
 	socketConsume (groupId, topic, event) {
 		const socketConsumer = this.Consumers[groupId];
@@ -65,8 +65,8 @@ class AtomicKafkaServer {
 
 	/**
 	* Produces messages from the local environment, this is used if data is pulled from any kind of database or store that is available on this server's host network. Does not require a websocket connection to retrieve data
-	* @data : JSON Object, the payload that is produced as a message to the Kafka cluster
-	* @topic : string, the topic that exists in the cluster that the producer will produce to
+	* @param {JSON Object} data: the payload that is produced as a message to the Kafka cluster
+	* @param {string} topic: the topic that exists in the cluster that the producer will produce to
 	*/
 	fileProduce(data, topic) {
 		const fileProducer = this.Producers[topic];
@@ -75,8 +75,8 @@ class AtomicKafkaServer {
 
 	/**
  	* Produces messages from a socket connection. This feature is implemented when the client produces messages to the server via a websocket connection
- 	* @event : string, the event that triggers the callback function for producing data to the topic
-	* @topic : string, the topic that exists in the cluster that the producer will produce to
+ 	* @param {string} event: the event that triggers the callback function for producing data to the topic
+	* @param {string} topic: the topic that exists in the cluster that the producer will produce to
 	*/
 	socketProduce(event, topic) {
 		const socketProducer = this.Producers[topic];
--- srv-lib/producer.js
+++ srv-lib/producer.js
@@ -13,12 +13,12 @@ class Producer{
 		*/
 		this.producer = kafka.producer();
 	}
-	
+
 	/**
 	* send a message to the Kafka cluster with the data passed in when socketProduce is invoked
 	* and use the topic provided by the user to send message
-  * @data : JSON Object, this data is sent as value to the value of the message going to the Kafka cluster
-	* @topic : String, specifies which topic in the kafka cluster to send the message
+  * @param {JSON Object} data: this data is sent as value to the value of the message going to the Kafka cluster
+	* @param {string} topic: specifies which topic in the kafka cluster to send the message
 	*/
 	executeSend = async (data, topic) => {
 		try {
@@ -40,8 +40,8 @@ class Producer{
 	/**
 	*	producer function: takes in data (as a JSON object) and a topic (as a string).
 	*	the producer connects to the kakfka cluster via the kafka broker that was required in and sends the data as the payload to the topic specified by the user
-	* @data : JSON Object, this data is sent as value to the value of the message going to the Kafka cluster
-	* @topic : string, specifies which topic in the kafka cluster to send the message
+	* @param {JSON Object} data: this data is sent as value to the value of the message going to the Kafka cluster
+	* @param {string} topic: specifies which topic in the kafka cluster to send the message
 	*/
 	produce = async (data, topic) => {
 		if(!data) return console.log('No data was passed in')
